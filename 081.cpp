e58b159f3edecf93c3ea74658de16d5cf12a8442
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 15:10:30 -0700

    Create 081.cpp

---
+++ 081.cpp
@@ -0,0 +1,54 @@
+/*
+081. Data Stream Median
+
+Numbers keep coming, return the median of numbers at every time a new number added.
+Have you met this question in a real interview?
+Example
+For numbers coming list: [1, 2, 3, 4, 5], return [1, 1, 2, 2, 3].
+For numbers coming list: [4, 5, 1, 3, 2, 6, 0], return [4, 4, 4, 3, 3, 3, 3].
+For numbers coming list: [2, 20, 100], return [2, 2, 20].
+Challenge
+Total run time in O(nlogn).
+Clarification
+What's the definition of Median? - Median is the number that in the middle of a sorted array. If there are n numbers in a sorted array A, the median is A[(n - 1) / 2]. For example, if A=[1,2,3], median is 2. If A=[1,19], median is 1.
+*/
+
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: The median of numbers
+     */
+    vector<int> medianII(vector<int> &nums) {
+        int n = nums.size();
+        priority_queue<int, vector<int>, less<int> > small;
+        priority_queue<int, vector<int>, greater<int> > large;
+        int i;
+        vector<int> ans;
+        
+        small.push(INT_MIN);
+        large.push(INT_MAX);
+        int vs;
+        int vl;
+        for(i = 0; i < n; ++i) {
+            if(i & 1) {
+                large.push(nums[i]);
+            } else {
+                small.push(nums[i]);
+            }
+            vs = small.top();
+            vl = large.top();
+            if(vs > vl) {
+                small.pop();
+                small.push(vl);
+                large.pop();
+                large.push(vs);
+            }
+            ans.push_back(small.top());
+        }
+        return ans;
+    }
+};
