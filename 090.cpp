62c78eb7632d86774c58e5bff7f9c2ccafe6f04f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 15:16:01 -0700

    Create 090.cpp

---
+++ 090.cpp
@@ -0,0 +1,85 @@
+/*
+090. k Sum II
+
+Given n unique integers, number k (1<=k<=n)  and target. Find all possible k integers where their sum is target.
+Have you met this question in a real interview?
+Example
+Given [1,2,3,4], k=2, target=5, [1,4] and [2,3] are possible solutions.
+*/
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return a list of lists of integer
+     */
+    vector<vector<int> > kSumII(vector<int> A, int k, int target) {
+        ans.clear();
+        v.clear();
+        sort(A.begin(), A.end());
+        v.resize(k);
+        this->n = A.size();
+        this->k = k;
+        this->target = target;
+        if (k == 1) {
+            solveOne(A, target);
+            return ans;
+        }
+        DFS(A, 0, 0, 0);
+        return ans;
+    }
+private:
+    vector<vector<int> > ans;
+    vector<int> v;
+    int n;
+    int k;
+    int target;
+    
+    void solveOne(vector<int> &A, int target) {
+        int i;
+        for (i = 0; i < n; ++i) {
+            if (A[i] == target) {
+                v[0] = target;
+                ans.push_back(v);
+            }
+        }
+    }
+    
+    void DFS(vector<int> &A, int idx, int sum, int cc) {
+        if (sum + A[idx] * (k - cc) > target) {
+            return;
+        }
+        if (sum + A[n - 1] * (k - cc) < target) {
+            return;
+        }
+        
+        if (cc == k - 2) {
+            int ll = idx;
+            int rr = n - 1;
+            while (ll < rr) {
+                if (A[ll] + A[rr] < target - sum) {
+                    ++ll;
+                } else if (A[ll] + A[rr] > target - sum) {
+                    --rr;
+                } else {
+                    v[k - 2] = A[ll];
+                    v[k - 1] = A[rr];
+                    ans.push_back(v);
+                    ++ll;
+                }
+            }
+            return;
+        }
+        
+        int i;
+        for (i = idx; i <= n - (k - cc); ++i) {
+            v[cc] = A[i];
+            DFS(A, i + 1, sum + A[i], cc + 1);
+        }
+    }
+};
