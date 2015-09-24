833fca4126e61b1c1a6c159bb24030ae53eccc06
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 17:02:05 -0700

    Create 130.cpp

---
+++ 130.cpp
@@ -0,0 +1,76 @@
+/*
+130. Heapify
+
+Given an integer array, heapify it into a min-heap array.
+For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
+Have you met this question in a real interview?
+Example
+Given [3,2,1,4,5], return [1,2,3,4,5] or any legal heap array.
+Challenge
+O(n) time complexity
+Clarification
+What is heap?
+Heap is a data structure, which usually have three methods: push, pop and top. where "push" add a new element the heap, "pop" delete the minimum/maximum element in the heap, "top" return the minimum/maximum element.
+
+What is heapify?
+Convert an unordered integer array into a heap array. If it is min-heap, for each element A[i], we will get A[i * 2 + 1] >= A[i] and A[i * 2 + 2] >= A[i].
+
+What if there is a lot of solutions?
+Return any of them.
+Given an integer array, heapify it into a min-heap array.
+For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
+Have you met this question in a real interview?
+Example
+Given [3,2,1,4,5], return [1,2,3,4,5] or any legal heap array.
+Challenge
+O(n) time complexity
+Clarification
+What is heap?
+Heap is a data structure, which usually have three methods: push, pop and top. where "push" add a new element the heap, "pop" delete the minimum/maximum element in the heap, "top" return the minimum/maximum element.
+
+What is heapify?
+Convert an unordered integer array into a heap array. If it is min-heap, for each element A[i], we will get A[i * 2 + 1] >= A[i] and A[i * 2 + 2] >= A[i].
+
+What if there is a lot of solutions?
+Return any of them.
+*/
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param A: Given an integer array
+     * @return: void
+     */
+    void heapify(vector<int> &A) {
+        int n = A.size();
+        if (n < 2) {
+            return;
+        }
+        int i, j, k;
+        int minVal;
+        
+        for (i = (n - 1) / 2; i >= 0; --i) {
+            j = i;
+            while (j * 2 + 1 <= n - 1) {
+                k = j;
+                minVal = A[j];
+                if (A[j * 2 + 1] < minVal) {
+                    k = j * 2 + 1;
+                    minVal = A[k];
+                }
+                if (j * 2 + 2 <= n - 1 && A[j * 2 + 2] < minVal) {
+                    k = j * 2 + 2;
+                    minVal = A[k];
+                }
+                if (k == j) {
+                    break;
+                }
+                swap(A[j], A[k]);
+                j = k;
+            }
+        }
+    }
+};
