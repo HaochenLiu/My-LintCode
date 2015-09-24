a5a51f30e6e597303053bbcc4512298867659cfe
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 17:47:41 -0700

    Create 382-3.cpp

---
+++ 382-3.cpp
@@ -0,0 +1,49 @@
+/*
+382. Triangle Count
+
+Given an array of integers, how many three numbers can be found in the array, so that we can build an triangle whose three edges length is the three numbers that we find?
+Have you met this question in a real interview?
+Example
+Given array S = [3,4,6,7], return 3. They are:
+[3,4,6]
+[3,6,7]
+[4,6,7]
+
+
+Given array S = [4,4,4,4], return 4. They are:
+[4(1),4(2),4(3)]
+[4(1),4(2),4(4)]
+[4(1),4(3),4(4)]
+[4(2),4(3),4(4)]
+*/
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    int triangleCount(vector<int> &S) {
+        sort(S.begin(), S.end());
+        
+        int i, j, k;
+        int ans = 0;
+        int n = S.size();
+        
+        for (i = 0; i < n; ++i) {
+            j = i + 1;
+            k = j + 1;
+            while (j < n - 1) {
+                while (k < n && S[k] - S[j] < S[i]) {
+                    ++k;
+                }
+                ans += k - j - 1;
+                ++j;
+            }
+        }
+        return ans;
+    }
+};
