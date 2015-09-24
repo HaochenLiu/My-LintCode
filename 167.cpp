56f23309b11294bd29de7fb1467d1004812ab8d6
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-23 17:23:30 -0700

    Create 167.cpp

---
+++ 167.cpp
@@ -0,0 +1,52 @@
+/*
+167. Add Two Numbers
+
+You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+Have you met this question in a real interview?
+Example
+Given 7->1->6 + 5->9->2. That is, 617 + 295.
+Return 2->1->9. That is 912.
+Given 3->1->5 and 5->9->2, return 8->0->8.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2
+     */
+    ListNode *addLists(ListNode *l1, ListNode *l2) {
+        // write your code here
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        int carry = 0;
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+        while(l1 || l2 || carry) {
+            if(l1) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            
+            if(l2) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            
+            cur->next = new ListNode(carry % 10);
+            carry /= 10;
+            cur = cur->next;
+        }
+
+        return dummy.next;
+    }
+};
