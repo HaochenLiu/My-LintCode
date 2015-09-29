8adae4772795a0e98dcbe62f4f1bf6b8b754695e
Haochen Liu <liuhaochen1221@hotmail.com>
2015-09-29 10:03:41 -0700

    Update 431.cpp

--- 431.cpp
+++ 431.cpp
@@ -30,14 +30,6 @@ public:
      * @param nodes a array of Undirected graph node
      * @return a connected set of a Undirected graph
      */
-    void dfs(vector<UndirectedGraphNode*> &nodes, vector<int> &path,
-            unordered_set<UndirectedGraphNode*> &visit, UndirectedGraphNode* n) {
-        visit.insert(n);
-        path.push_back(n->label);
-        for (auto &nn : n->neighbors) if (visit.find(nn) == visit.end()) {
-            dfs(nodes, path, visit, nn);
-        }
-    }
     vector<vector<int>> connectedSet(vector<UndirectedGraphNode*>& nodes) {
         // Write your code here
         unordered_set<UndirectedGraphNode*> visit;
@@ -49,8 +41,19 @@ public:
                 dfs(nodes, path, visit, n);
                 sort(path.begin(), path.end());
                 res.push_back(path);
-            }
+            } 
         }
         return res;
+    }
+private:
+    void dfs(vector<UndirectedGraphNode*> &nodes, vector<int> &path,
+            unordered_set<UndirectedGraphNode*> &visit, UndirectedGraphNode* n) {
+        visit.insert(n);
+        path.push_back(n->label);
+        for (auto &nn : n->neighbors) {
+            if (visit.find(nn) == visit.end()) {
+                dfs(nodes, path, visit, nn);
+            }
+        }
     }
 };
