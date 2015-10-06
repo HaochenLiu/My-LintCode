402799a47ba70c409707629e299eaf9e47a4846f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-06 11:56:03 -0700

    Update 433-union_find.cpp

--- 433-union_find.cpp
+++ 433-union_find.cpp
@@ -11,6 +11,7 @@ Given graph:
   [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 1]
 ]
+
 return 3.
 Note
 0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
