f77186ceeb117c566c5a39055bb869d10147ad8e
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-06 11:55:38 -0700

    Update 433.cpp

--- 433.cpp
+++ 433.cpp
@@ -21,63 +21,48 @@ Note
 
 class Solution {
 public:
-    Solution() {
-        d.resize(4, vector<int>(2));
-        int i;
-        d[0][0] = -1;
-        d[0][1] = 0;
-        d[1][0] = +1;
-        d[1][1] = 0;
-        d[2][0] = 0;
-        d[2][1] = -1;
-        d[3][0] = 0;
-        d[3][1] = +1;
-    }
     /**
      * @param grid a boolean 2D matrix
      * @return an integer
      */
-    int numIslands(vector<vector<bool> > &grid) {
-        n = grid.size();
-        if (n == 0) {
-            return 0;
-        }
-        m = grid[0].size();
-        if (m == 0) {
-            return 0;
-        }
-        int ans = 0;
-        int i, j;
-        
-        b.resize(n, vector<bool>(m));
-        for (i = 0; i < n; ++i) {
-            for (j = 0; j < m; ++j) {
-                if (grid[i][j] && !b[i][j]) {
-                    ++ans;
-                    DFS(i, j, grid);
+    int numIslands(vector<vector<bool>> &grid) {
+        // Write your code here
+        if(grid.empty() || grid[0].empty()) return 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int res = 0;
+
+        vector<vector<bool>> visited(n, vector<bool>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] && !visited[i][j]) {
+                    res++;
+                    dfs(grid, visited, i, j, n, m);
                 }
             }
         }
-        b.clear();
-        return ans;
+
+        return res;
     }
 private:
-    vector<vector<bool> > b;
-    vector<vector<int> > d;
-    int n, m;
-   
-    void DFS(int x, int y, vector<vector<bool> > &grid) {
-        b[x][y] = true;
-        int x1, y1;
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    bool inBound(int x, int y, int n, int m) {
+        return (x >= 0 && y >= 0 && x < n && y < m);
+    }
+    
+    void dfs(vector<vector<bool>> &grid, vector<vector<bool>>& visited, int x, int y, int n, int m) {
+        visited[x][y] = true;
+        int nx;
+        int ny;
         int i;
-        for (i = 0; i < 4; ++i) {
-            x1 = x + d[i][0];
-            y1 = y + d[i][1];
-            if (x1 < 0 || x1 > n - 1 || y1 < 0 || y1 > m - 1) {
-                continue;
-            }
-            if (grid[x1][y1] && !b[x1][y1]) {
-                DFS(x1, y1, grid);
+        for (i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if(!inBound(nx, ny, n, m)) continue;
+            if(grid[nx][ny] && !visited[nx][ny]) {
+                dfs(grid, visited, nx, ny, n, m);
             }
         }
     }
