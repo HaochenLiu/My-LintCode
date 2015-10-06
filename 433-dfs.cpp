a9533ae30b42bf509d0a27e5b6c4fe63898245b6
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-06 13:44:16 -0700

    Update 433-dfs.cpp

--- 433-dfs.cpp
+++ 433-dfs.cpp
@@ -18,6 +18,13 @@ Note
 0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
 */
 
+/*
+Time: O(nm)
+Space: O(nm)
+Extra space: O(nm)
+Stack depth: O(nm)
+*/
+
 class Solution {
 public:
     /**
