2696521a1fbe5e991b82759814c36fdc5683ed98
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-17 11:33:37 -0700

    Create 453-1.cpp

---
+++ 453-1.cpp
@@ -0,0 +1,64 @@
+/*
+453. Flatten Binary Tree to Linked List
+
+Flatten a binary tree to a fake "linked list" in pre-order traversal.
+
+Here we use the right pointer in TreeNode as the next pointer in ListNode.
+
+Have you met this question in a real interview? Yes
+Example
+              1
+               \
+     1          2
+    / \          \
+   2   5    =>    3
+  / \   \          \
+ 3   4   6          4
+                     \
+                      5
+                       \
+                        6
+Note
+Don't forget to mark the left child of each node to null. Or you will get Time Limit Exceeded or Memory Limit Exceeded.
+
+Challenge
+Do it in-place without any extra memory.
+*/
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void flatten(TreeNode *root) {
+        // write your code here
+        if(root == NULL) return;
+        if(root->left == NULL && root->right == NULL) return;
+        flatten(root->left);
+        flatten(root->right);
+        if(root->left) {
+            TreeNode* node = root->left;
+            while(node->right) {
+                node = node->right;
+            }
+            node->right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+        }
+
+        return;
+    }
+};
