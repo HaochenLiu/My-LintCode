e4362df04eaaa3253a4f44974980289ffb02f2a8
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-18 11:50:51 -0700

    Update and rename 390-1.cpp to 390-2.cpp

--- 390-2.cpp
+++ 390-2.cpp
@@ -31,6 +31,11 @@ Solve it in O(n+m) time.
 If you come up with an algorithm that you thought it is O(n log m) or O(m log n), can you prove it is actually O(n+m) or propose a similar but O(n+m) algorithm?
 */
 
+/*
+Time:  O(nlogm)
+Space: O(1)
+*/
+
 class Solution {
 private:
     bool isPeak(vector<vector<int>>& A, int x, int y) {
