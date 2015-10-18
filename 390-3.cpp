0f39f1969bce8213ce4281beb82b766626d6f666
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-18 11:50:17 -0700

    Update and rename 390-2.cpp to 390-3.cpp

--- 390-3.cpp
+++ 390-3.cpp
@@ -31,8 +31,10 @@ Solve it in O(n+m) time.
 If you come up with an algorithm that you thought it is O(n log m) or O(m log n), can you prove it is actually O(n+m) or propose a similar but O(n+m) algorithm?
 */
 
-// Time:  O(m + n)
-// Space: O(1)
+/*
+Time:  O(m + n)
+Space: O(1)
+*/
 
 class Solution {
 public:
