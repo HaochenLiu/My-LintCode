522450d5217b5f4db852275298e3638935f5e05e
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-18 20:45:55 -0700

    Update 402.cpp

--- 402.cpp
+++ 402.cpp
@@ -7,3 +7,42 @@ Example
 Give [-3, 1, 3, -3, 4], return [1,4].
 */
 
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return  A list of integers includes the index of
+     *          the first number and the index of the last number
+     */
+    vector<int> continuousSubarraySum(vector<int>& A) {
+        vector<int> resLocal;
+        vector<int> resGlobal;
+        if(A.empty()) return resLocal;
+        int n = A.size();
+        int maxLocal = A[0];
+        int maxGlobal = A[0];
+        resLocal.push_back(0);
+        resLocal.push_back(0);
+        resGlobal.push_back(0);
+        resGlobal.push_back(0);
+
+        for(int i = 1; i < n; i++) {
+            if(maxLocal > 0) {
+                maxLocal += A[i];
+                resLocal[1] = i;
+            } else {
+                maxLocal = A[i];
+                resLocal[0] = i;
+                resLocal[1] = i;
+            }
+            
+            if(maxLocal > maxGlobal) {
+                resGlobal[0] = resLocal[0];
+                resGlobal[1] = resLocal[1];
+                maxGlobal = maxLocal;
+            }
+        }
+
+        return resGlobal;
+    }
+};
