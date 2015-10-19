c1b7054c509c58b9047ff5311b7ed90700a8bdc9
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-18 20:59:26 -0700

    Update 403-1.cpp

--- 403-1.cpp
+++ 403-1.cpp
@@ -8,113 +8,79 @@ Give [3, 1, -100, -3, 4], return [4,1].
 */
 
 class Solution {
+private:
+    vector<int> continuousSubarraySum(vector<int>& A) {
+        vector<int> resLocal;
+        vector<int> resGlobal;
+        if(A.empty()) return resLocal;
+        int n = A.size();
+        int maxLocal = A[0];
+        int maxGlobal = A[0];
+        resLocal.push_back(0);
+        resLocal.push_back(0);
+        resGlobal.push_back(0);
+        resGlobal.push_back(0);
+
+        for(int i = 1; i < n; i++) {
+            if(maxLocal > 0) {
+                maxLocal += A[i];
+                resLocal[1] = i;
+            } else {
+                maxLocal = A[i];
+                resLocal[0] = i;
+                resLocal[1] = i;
+            }
+            
+            if(maxLocal > maxGlobal) {
+                resGlobal[0] = resLocal[0];
+                resGlobal[1] = resLocal[1];
+                maxGlobal = maxLocal;
+            }
+        }
+
+        return resGlobal;
+    }
+
 public:
     /**
      * @param A an integer array
-     * @return  A list of integers includes the index of
+     * @return  A list of integers includes the index of 
      *          the first number and the index of the last number
      */
     vector<int> continuousSubarraySumII(vector<int>& A) {
-        int msum1;
-        vector<int> ans1;
-        ans1 = continuousSubarraySum(A, msum1);
-        if (ans1[0] == ans1[1]) {
-            return ans1;
-        }
-        
-        vector<int> s1;
-        vector<int> i1;
-        vector<int> s2;
-        vector<int> i2;
-        
+        // Write your code here
+        vector<int> resMax;
+        vector<int> resMin;
+        if(A.empty()) return resMax;
         int n = A.size();
-        int i;
-        int sum;
-        
-        s1.resize(n);
-        i1.resize(n);
-        s2.resize(n);
-        i2.resize(n);
-        
-        sum = A[0];
-        s1[0] = A[0];
-        i1[0] = 0;
-        for (i = 1; i <= n - 1; ++i) {
+        resMax = continuousSubarraySum(A);
+
+        int maxVal = 0;
+        for(int i = resMax[0]; i <= resMax[1]; i++) {
+            maxVal += A[i];
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
             sum += A[i];
-            if (sum > s1[i - 1]) {
-                s1[i] = sum;
-                i1[i] = i;
-            } else {
-                s1[i] = s1[i - 1];
-                i1[i] = i1[i - 1];
-            }
+            A[i] = -A[i];
         }
-        
-        sum = A[n - 1];
-        s2[n - 1] = A[n - 1];
-        i2[n - 1] = n - 1;
-        for (i = n - 2; i >= 0; --i) {
-            sum += A[i];
-            if (sum > s2[i + 1]) {
-                s2[i] = sum;
-                i2[i] = i;
-            } else {
-                s2[i] = s2[i + 1];
-                i2[i] = i2[i + 1];
-            }
+
+        int minVal = 0;
+        resMin = continuousSubarraySum(A);
+        for(int i = resMin[0]; i <= resMin[1]; i++) {
+            minVal -= A[i];
         }
-        
-        int msum2 = 0;
-        vector<int> ans2;
-        ans2.resize(2);
-        for (i = 0; i < n - 1; ++i) {
-            if (s1[i] + s2[i + 1] > msum2) {
-                msum2 = s1[i] + s2[i + 1];
-                ans2[0] = i2[i + 1];
-                ans2[1] = i1[i];
-            }
+
+        if(sum == minVal) {
+            return resMax;
+        } else if(maxVal >= sum - minVal) {
+            return resMax;
+        } else {
+            resMin[0]--;
+            resMin[1]++;
+            swap(resMin[0], resMin[1]);
+            return resMin;
         }
-        return msum1 > msum2 ? ans1 : ans2;
-    }
-private:
-    vector<int> continuousSubarraySum(vector<int>& A, int &msum) {
-        int n = A.size();
-        int ll;
-        int sum;
-        
-        ll = 0;
-        msum = A[0];
-        int i;
-        for (i = 1; i < n; ++i) {
-            if (A[i] > msum) {
-                msum = A[i];
-                ll = i;
-            }
-        }
-        vector<int> ans;
-        if (msum <= 0) {
-            ans.push_back(ll);
-            ans.push_back(ll);
-            return ans;
-        }
-        
-        int mll, mrr;
-        ll = 0;
-        msum = sum = 0;
-        for (i = 0; i < n; ++i) {
-            sum += A[i];
-            if (sum < 0) {
-                sum = 0;
-                ll = i + 1;
-            }
-            if (sum > msum) {
-                msum = sum;
-                mll = ll;
-                mrr = i;
-            }
-        }
-        ans.push_back(mll);
-        ans.push_back(mrr);
-        return ans;
     }
 };
