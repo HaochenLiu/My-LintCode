60b7e43a7fe1b07d3a17439d8215d7f8cca83fff
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-20 01:47:49 -0700

    Update 247.cpp

--- 247.cpp
+++ 247.cpp
@@ -22,48 +22,37 @@ It is much easier to understand this problem if you finished Segment Tree Builda
 */
 
 /**
-* Definition of SegmentTreeNode:
-* class SegmentTreeNode {
-* public:
-*     int start, end, count;
-*     SegmentTreeNode *left, *right;
-*     SegmentTreeNode(int start, int end, int count) {
-*         this->start = start;
-*         this->end = end;
-*         this->count = count;
-*         this->left = this->right = NULL;
-*     }
-* }
-*/
-typedef SegmentTreeNode STN;
+ * Definition of SegmentTreeNode:
+ * class SegmentTreeNode {
+ * public:
+ *     int start, end, count;
+ *     SegmentTreeNode *left, *right;
+ *     SegmentTreeNode(int start, int end, int count) {
+ *         this->start = start;
+ *         this->end = end;
+ *         this->count = count;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
 class Solution {
 public:
     /**
-     *@param root, start, end: The root of segment tree and
+     *@param root, start, end: The root of segment tree and 
      *                         an segment / interval
-     *@return: The count number in the interval [start, end]
+     *@return: The count number in the interval [start, end] 
      */
-    int query(STN *root, int start, int end) {
-        if (root == NULL) {
-            return 0;
-        }
-        if (start > root->end || end < root->start || start > end) {
-            return 0;
-        }
-        if (root->start == root->end) {
-            return root->count;
-        }
-        int mid = root->start + (root->end - root->start) / 2;
-        if (end <= mid) {
-            return query(root->left, start, end);
-        }
-        if (start >= mid + 1) {
-            return query(root->right, start, end);
-        }
-        int c1 = root->start == start ? root->left->count :
-                 query(root->left, start, mid);
-        int c2 = root->end == end ? root->right->count :
-                 query(root->right, mid + 1, end);
-        return c1 + c2;
+    int query(SegmentTreeNode *root, int start, int end) {
+        // write your code here
+        if(root == NULL) return 0;
+        if(start > end) return 0;
+        if(start > root->end) return 0;
+        if(end < root->start) return 0;
+        start = max(start, root->start);
+        end = min(end, root->end);
+        if(start == root->start && end == root->end) return root->count;
+        int t1 = query(root->left, start, min(end, root->left->end));
+        int t2 = query(root->right, max(start, root->right->start), end);
+        return t1 + t2;
     }
 };
