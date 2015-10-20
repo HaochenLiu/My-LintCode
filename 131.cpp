839c2a0c9d8b539f120b9cd0b8f57cc86c8bd85f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-20 10:49:22 -0700

    Update 131.cpp

--- 131.cpp
+++ 131.cpp
@@ -26,97 +26,43 @@ Note
 Please merge the adjacent outlines if they have the same height and make sure different outlines cant overlap on x-axis.
 */
 
-#include <algorithm>
-#include <map>
-using namespace std;
-
-struct Term {
-    int x, y;
-    int op;
-    bool operator < (const Term &t) const {
-        if (x != t.x) {
-            return x < t.x;
-        }
-        if (op != t.op) {
-            return op < t.op;
-        }
-        if (op == 0) {
-            // Start of a building
-            return y > t.y;
-        } else {
-            // End of a building
-            return y < t.y;
-        }
-    }
-};
-
 class Solution {
 public:
     /**
      * @param buildings: A list of lists of integers
      * @return: Find the outline of those buildings
      */
-    vector<vector<int> > buildingOutline(vector<vector<int> > &buildings) {
-        vector<vector<int> > &a = buildings;
-        vector<Term> v;
-        Term t;
-        int n = a.size();
-        int i;
-        for (i = 0; i < n; ++i) {
-            t.x = a[i][0];
-            t.y = a[i][2];
-            t.op = 0;
-            v.push_back(t);
-            t.x = a[i][1];
-            t.y = a[i][2];
-            t.op = 1;
-            v.push_back(t);
+    vector<vector<int>> buildingOutline(vector<vector<int>>& buildings) {
+        // write your code here
+        vector<vector<int>> res;
+        if(buildings.empty()) return res;
+        int n = buildings.size();
+        vector<pair<int, int>> height;
+        for(int i = 0; i < n; i++) {
+            height.push_back(make_pair(buildings[i][0], -buildings[i][2]));
+            height.push_back(make_pair(buildings[i][1], buildings[i][2]));
         }
-        sort(v.begin(), v.end());
-        
-        map<int, int> m;
-        vector<int> px, py;
-        bool b;
-        
-        m[0] = 1;
-        n = v.size();
-        for (i = 0; i < n; ++i) {
-            if (v[i].op == 0) {
-                // Start of a building
-                if (m.rbegin()->first < v[i].y) {
-                    px.push_back(v[i].x);
-                    py.push_back(v[i].y);
-                }
-                ++m[v[i].y];
+        sort(height.begin(), height.end());
+        multiset<int> s;
+        s.insert(0);
+        int preHeight = 0;
+        int curHeight = 0;
+        int prePos = 0;
+        for(int i = 0; i < 2 * n; i++) {
+            if(height[i].second < 0) {
+                s.insert(-height[i].second);
             } else {
-                // End of a building
-                b = false;
-                --m[v[i].y];
-                if (m[v[i].y] == 0) {
-                    b = v[i].y == m.rbegin()->first;
-                    m.erase(v[i].y);
-                }
-                if (b) {
-                    px.push_back(v[i].x);
-                    py.push_back(m.rbegin()->first);
-                }
+                s.erase(s.find(height[i].second));
             }
+            curHeight = *s.rbegin();
+            if(preHeight == curHeight) continue;
+            if(preHeight != 0) {
+                res.push_back({prePos, height[i].first, preHeight});
+            }
+            preHeight = curHeight;
+            prePos = height[i].first;
         }
         
-        n = px.size();
-        vector<vector<int> > ans;
-        vector<int> seg(3);
-        for (i = 0; i < n - 1; ++i) {
-            // Ignore all zeros
-            if (py[i] == 0) {
-                continue;
-            }
-            seg[0] = px[i];
-            seg[1] = px[i + 1];
-            seg[2] = py[i];
-            ans.push_back(seg);
-        }
-        
-        return ans;
+        return res;
     }
 };
