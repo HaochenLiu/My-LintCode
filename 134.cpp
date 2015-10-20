ff72005978be82716b72f7bdf26c042e2c83dd0f
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-20 10:57:01 -0700

    Update 134.cpp

--- 134.cpp
+++ 134.cpp
@@ -8,93 +8,57 @@ Have you met this question in a real interview?
 Example
 */
 
-// Hashmap and doubly linked list.
-#include <unordered_map>
-using namespace std;
+#include<list>
 
-typedef struct DoublyListNode {
+struct CacheNode{
     int key;
-    int val;
-    struct DoublyListNode *left;
-    struct DoublyListNode *right;
-    DoublyListNode(int key = 0, int val = 0): key(key), val(val) {
-        left = right = NULL;
-    }
-} DLN;
+    int value;
+    CacheNode(int k , int v) : key(k), value(v){}
+};
 
 class LRUCache{
 public:
     // @param capacity, an integer
     LRUCache(int capacity) {
-        cap = capacity;
-        size = 0;
-        head = tail = NULL;
+        // write your code here
+        size = capacity;
     }
-   
+    
     // @return an integer
     int get(int key) {
-        if (um.find(key) == um.end()) {
-            return DEFAULT_VALUE;
+        // write your code here
+        if(cacheMap.find(key) != cacheMap.end()) {
+            auto it = cacheMap[key];
+            cacheList.splice(cacheList.begin(), cacheList , it);
+            cacheMap[key] = cacheList.begin();
+            return cacheList.begin()->value;
+        } else {
+            return -1;
         }
-        moveToFront(key);
-        return um[key]->val;
     }
 
     // @param key, an integer
     // @param value, an integer
     // @return nothing
     void set(int key, int value) {
-        DLN *p;
-        if (um.find(key) == um.end()) {
-            if (size == cap) {
-                p = tail;
-                um.erase(tail->key);
-                tail->key = key;
-                tail->val = value;
-            } else {
-                p = new DLN(key, value);
-                if (head == NULL) {
-                    head = tail = p;
-                } else {
-                    p->right = head;
-                    head->left = p;
-                    head = p;
-                }
-                ++size;
+        // write your code here
+        if (cacheMap.find(key) != cacheMap.end()) {
+            auto it = cacheMap[key];
+            cacheList.splice(cacheList.begin(), cacheList , it);
+            cacheMap[key] = cacheList.begin();
+            cacheList.begin()->value = value;
+        } else {
+            if(cacheList.size() == size) {
+                cacheMap.erase(cacheList.back().key);
+                cacheList.pop_back();
             }
-            um[key] = p;
-        } else {
-            p = um[key];
-            p->val = value;
+            cacheList.push_front(CacheNode(key, value));
+            cacheMap[key] = cacheList.begin();
         }
-        moveToFront(key);
     }
+
 private:
-    static const int DEFAULT_VALUE = -1;
-    unordered_map<int, DLN *> um;
-    int cap;
     int size;
-    DLN *head, *tail;
-   
-    void moveToFront(int key) {
-        if (um.find(key) == um.end()) {
-            return;
-        }
-        DLN *p = um[key];
-        if (p == head) {
-            return;
-        }
-        
-        if (p == tail) {
-            tail = tail->left;
-            tail->right = NULL;
-        } else {
-            p->left->right = p->right;
-            p->right->left = p->left;
-        }
-        p->left = NULL;
-        p->right = head;
-        head->left = p;
-        head = p;
-    }
+    list<CacheNode> cacheList;
+    unordered_map<int, list<CacheNode>::iterator> cacheMap;
 };
