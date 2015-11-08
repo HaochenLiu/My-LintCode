f831ae5e2c34892bbee1a85e7a4b96278cc91096
Haochen Liu <liuhaochen1221@hotmail.com>
2015-11-07 16:36:09 -0800

    Update 435.cpp

--- 435.cpp
+++ 435.cpp
@@ -21,30 +21,27 @@ public:
      */
     int postOffice(vector<int>& A, int k) {
         const int n = A.size();
-        if (A.empty() || k >= n) {
-            return 0;
-        }
-
-        sort(A.begin(), A.end());  // Time:  O(nlogn)
+        if (A.empty() || k >= n) return 0;
+        sort(A.begin(), A.end());
 
         // Precompute cost.
         // Time:  O(n^3)
         // Space: O(n^2)
-        vector<vector<int>> cost(A.size() + 1, vector<int>(A.size() + 1, 0));
-        computeMinCost(A, &cost);
+        vector<vector<int>> cost(n + 1, vector<int>(n + 1, 0));
+        computeMinCost(A, cost);
 
         // DP of sum.
         // Time:  O(k * n^2)
         // Space: O(n)
         // sum[i][j] denotes the smallest sum of
         // picking i post offices for the first j houses.
-        vector<vector<int>> sum(2, vector<int>(A.size() + 1, INT_MAX));
+        vector<vector<int>> sum(2, vector<int>(n + 1, INT_MAX));
         sum[0][0] = 0;
-        for (int i = 1; i <= k; ++i) {
-            for (int j = 0; j <= n; ++j) {
+        for(int i = 1; i <= k; i++) {
+            for(int j = 0; j <= n; j++) {
                 // sum[i % 2][j] = INT_MAX;
-                for (int r = 1; r <= j; ++r) {
-                    if (sum[(i - 1) % 2][j - r] != INT_MAX) {
+                for(int r = 1; r <= j; r++) {
+                    if(sum[(i - 1) % 2][j - r] != INT_MAX) {
                         sum[i % 2][j] = min(sum[i % 2][j],
                                             sum[(i - 1) % 2][j - r] +
                                             cost[j - r + 1][j]);
@@ -55,18 +52,18 @@ public:
         return sum[k % 2][n];
     }
 
-    void computeMinCost(const vector<int>& A, vector<vector<int>> *cost) {
+    void computeMinCost(const vector<int>& A, vector<vector<int>>& cost) {
         // Min cost of building a post office between house (i, j).
         // This post office must be in median position.
         const int n = A.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
+        for(int i = 0; i < n; ++i) {
+            for(int j = i; j < n; ++j) {
                 int mid = (i + j) / 2;
-                for (int r = i; r <= mid; ++r) {
-                    (*cost)[i + 1][j + 1] += A[mid] - A[r];
+                for(int l = i; l <= mid; l++) {
+                    cost[i + 1][j + 1] += A[mid] - A[l];
                 }
-                for (int r = mid + 1; r <= j; ++r) {
-                    (*cost)[i + 1][j + 1] += A[r] - A[mid];
+                for(int r = mid + 1; r <= j; r++) {
+                    cost[i + 1][j + 1] += A[r] - A[mid];
                 }
             }
         }
