4adae1dfeb30f711827e82195b92149c7cdd7bcf
Haochen Liu <liuhaochen1221@hotmail.com>
2015-12-21 16:15:21 -0800

    Update and rename Graph_Valid_Tree.cpp to 178.cpp

--- 178.cpp
+++ 178.cpp
@@ -1,5 +1,5 @@
 /*
-Graph Valid Tree
+178. Graph Valid Tree
 
 Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
 Have you met this question in a real interview?
